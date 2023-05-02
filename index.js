1bc3886962e0022f0d9e20deec8783699b0bca81
FOYSAL AHMED MiN <foysalahmedmin@gmail.com>
2023-05-03 02:50:37 +0600

    set sever respons for all chefs info

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const chefs_info = require('./Data/data.json')
+const app = express();
 const port = 55555
 
 app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(chefs_info);
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
