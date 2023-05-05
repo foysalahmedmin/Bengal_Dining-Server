1233c676a6c27e737d67426036dabb6a19480fda
FOYSAL AHMED MiN <foysalahmedmin@gmail.com>
2023-05-05 19:51:50 +0600

    update server with review data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const chefs_info = require('./Data/data.json')
+const reviews_info = require('./Data/reviews.json')
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000 ;
@@ -15,6 +16,10 @@ app.get('/chef/:id', (req, res) => {
   const id = req.params.id;
   const chef = chefs_info.find(x => x.id === +id)
   res.send(chef);
+})
+
+app.get('/reviews', (req, res) => {
+  res.send(reviews_info);
 })
 
 app.listen(port, () => {
